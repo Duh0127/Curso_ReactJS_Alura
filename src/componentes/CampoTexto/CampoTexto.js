d392693e6ca063ccf19d82ad1da2832936ab08f0
Duh0127 <rodatupmocgamer22@gmail.com>
2023-06-09 16:55:16 -0300

    Manipulando o input do usuário

--- src/componentes/CampoTexto/CampoTexto.js
+++ src/componentes/CampoTexto/CampoTexto.js
@@ -1,10 +1,18 @@
 import './CampoTexto.css';
 
 export const CampoTexto = (props) => {
+
+    let valor = '';
+
+    const aoDigitado = (evento)=>{
+        valor = evento.target.value;
+        console.log(valor);
+    }
+
     return(
         <div className='campo-texto'>
             <label>{props.label}</label>
-            <input required={props.obrigatorio} type="text" placeholder={props.placeholder}/>
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} type="text" placeholder={props.placeholder}/>
         </div>
     )
 }
--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -1,10 +1,8 @@
 import './ListaSuspensa.css';
 
 const ListaSuspensa = (props)=>{
-    console.log(props.itens);
-
+    // console.log(props.itens);
     //MAP faz o mapeamento do array, transforma em uma lista diferente e dps te retorna
-
     return(
         <div className='lista-suspensa'>
             <label>{props.label}</label>
