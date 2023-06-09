d9366f21cd063947a261dfcba764661006d014d0
Duh0127 <rodatupmocgamer22@gmail.com>
2023-06-09 14:35:05 -0300

    Criação do CampoTexto

---
+++ src/componentes/CampoTexto/CampoTexto.js
@@ -0,0 +1,10 @@
+import './CampoTexto.css';
+
+export const CampoTexto = () => {
+    return(
+        <div className='campo-texto'>
+            <label>Nome:</label>
+            <input type="text" />
+        </div>
+    )
+}
